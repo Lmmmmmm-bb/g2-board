e0a93fd1fc8d9e126a36b2119d165f2812060c27
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-08-14 20:07:53 +0800

    chore: setup app description

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,6 +7,7 @@ const App: FC = () => {
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.header}>G2 Playground</h2>
+      <p className={styles.description}>G2 实在是太难玩了！</p>
       <div className={styles.linkWrapper}>
         <Link to='/sankey'>桑基图 Sankey</Link>
       </div>
